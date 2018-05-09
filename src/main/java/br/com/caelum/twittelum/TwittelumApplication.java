2d1477917894c3a982db809b5cb0560ce71991be
Matheus Brandino <m.v.brandino@gmail.com>
2018-05-08 21:42:11 -0300

    config inicial do projeto

---
+++ src/main/java/br/com/caelum/twittelum/TwittelumApplication.java
@@ -0,0 +1,12 @@
+package br.com.caelum.twittelum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwittelumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwittelumApplication.class, args);
+    }
+}
---
+++ src/test/java/br/com/caelum/twittelum/TwittelumApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.caelum.twittelum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwittelumApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
