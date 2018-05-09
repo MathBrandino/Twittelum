471659a1c3df0a69d4e91eb06bd5145f66b3c1b5
Matheus Brandino <m.v.brandino@gmail.com>
2018-05-08 21:43:13 -0300

    criando dao para tweet

---
+++ src/main/java/br/com/caelum/twittelum/dao/TweetDao.java
@@ -0,0 +1,18 @@
+package br.com.caelum.twittelum.dao;
+
+import br.com.caelum.twittelum.modelo.Tweet;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.Repository;
+
+public interface TweetDao extends Repository<Tweet, Long> {
+
+
+    List<Tweet> findAll();
+
+    Optional<Tweet> findById(Long id);
+
+    Tweet save(Tweet tweet);
+}
