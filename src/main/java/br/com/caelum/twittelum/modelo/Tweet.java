8dddea5409dee627a5e773079d5121479cc4b8e6
Matheus Brandino <m.v.brandino@gmail.com>
2018-05-10 16:52:18 -0300

    colocando regra de tamanho no conteudo

--- src/main/java/br/com/caelum/twittelum/modelo/Tweet.java
+++ src/main/java/br/com/caelum/twittelum/modelo/Tweet.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +20,7 @@ public class Tweet {
     private Long id;
     @ManyToOne
     private Usuario dono;
+    @Size(max = 240)
     private String conteudo;
     private String foto;
     @OneToMany(mappedBy = "tweet", cascade = CascadeType.REMOVE)
