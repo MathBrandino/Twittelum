62f8b26151d8d48aec8f92951c4cb5e3b040f62e
Matheus Brandino <m.v.brandino@gmail.com>
2018-05-10 16:45:14 -0300

    corrigindo imports

--- src/main/java/br/com/caelum/twittelum/controller/CurtidaController.java
+++ src/main/java/br/com/caelum/twittelum/controller/CurtidaController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.sun.org.apache.regexp.internal.RE;
 
 @RestController
 @RequestMapping("/curtida")
