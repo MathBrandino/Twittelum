f12b68da3567369004a95131cb73b37fde2dd1f7
Matheus Brandino <m.v.brandino@gmail.com>
2018-06-12 15:22:06 -0300

    fazendo a busca ser ordenada por data

--- src/main/java/br/com/caelum/twittelum/dao/TweetDao.java
+++ src/main/java/br/com/caelum/twittelum/dao/TweetDao.java
@@ -5,11 +5,12 @@ import br.com.caelum.twittelum.modelo.Tweet;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 public interface TweetDao extends Repository<Tweet, Long> {
 
-
+    @Query("select t from Tweet t order by data desc")
     List<Tweet> findAll();
 
     Optional<Tweet> findById(Long id);
