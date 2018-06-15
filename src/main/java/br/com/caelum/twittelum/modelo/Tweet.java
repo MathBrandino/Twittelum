05ba2a66d8f97309cffc8c16753a050bb5bd80ab
Matheus Brandino <m.v.brandino@gmail.com>
2018-06-14 21:54:09 -0300

    fazendo a foto ser um lob, porque é grande para caramba

--- src/main/java/br/com/caelum/twittelum/modelo/Tweet.java
+++ src/main/java/br/com/caelum/twittelum/modelo/Tweet.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
@@ -22,6 +23,7 @@ public class Tweet {
     private Usuario dono;
     @Size(max = 240)
     private String conteudo;
+    @Lob
     private String foto;
     @OneToMany(mappedBy = "tweet", cascade = CascadeType.REMOVE)
     private List<Curtida> curtidas = new ArrayList<>();
